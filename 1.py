82fc50638582bc43f261c3ed0ac2eae8dd4cdc0c
Oksana Tarnavska <oxy_101@ukr.net>
2023-05-17 12:59:11 +0300

    all_visual_added

--- 1.py
+++ 1.py
@@ -8,8 +8,11 @@ W = 1150
 H = 900
 screen = pg.display.set_mode((W, H))
 
+# define colors
 BG = 0, 25, 51
 cell_color = 0, 51, 102
+dark_red1 = 102, 0, 0
+dark_green1 = 0, 102, 0
 
 # cell side size
 a = 90
@@ -45,24 +48,37 @@ class Cell(pg.sprite.Sprite):
         self.rect.x = x0
         self.rect.y = y0
         self.clicked = False
+        self.status = None
 
     def update(self, ev, pos):
-        #print(self.clicked)
+        global turn
         # if someone clicks the mouse button
         if ev.type == pg.MOUSEBUTTONDOWN:
             # if mouse cursor is in the cell
-            if self.rect.x < pos[0] < self.rect.x + a and self.rect.y < pos[1] < self.rect.y + a:
+            if self.rect.x < pos[0] < self.rect.x + a and self.rect.y < pos[1] < self.rect.y + a and not self.clicked:
                 self.clicked = True
+                turn *= -1
+                if turn == 1:
+                    self.status = 1
+                else:
+                    self.status = 2
 
     def draw(self):
         if self.clicked:
-            self.image = x
+            if self.status == 1:
+                self.image = x
+            else:
+                self.image = o
         screen.blit(self.image, self.rect)
 
     def lightning(self, pos):
+        global turn
         if not self.clicked:
             if self.rect.x < pos[0] < self.rect.x + a and self.rect.y < pos[1] < self.rect.y + a:
-                pg.draw.rect(screen, (255, 255, 255), pg.Rect(self.rect.x, self.rect.y, a, a))
+                if turn == -1:
+                    pg.draw.rect(screen, dark_red1, pg.Rect(self.rect.x, self.rect.y, a, a))
+                elif turn == 1:
+                    pg.draw.rect(screen, dark_green1, pg.Rect(self.rect.x, self.rect.y, a, a))
 
 
 cell_group = pg.sprite.Group()
@@ -70,6 +86,7 @@ for i in range(9):
     cell = Cell(cell_list[i][0], cell_list[i][1])
     cell_group.add(cell)
 
+turn = -1
 
 run = True
 
